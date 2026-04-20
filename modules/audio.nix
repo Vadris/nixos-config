8938b33a0f0d8ba45747d4f681711b7d9a94b5ce
Fynn Jansen <fynn@jansen-home.de>
2026-04-20 22:18:44 +0200

    Actually put the correct stuff in the audio configuration

--- modules/audio.nix
+++ modules/audio.nix
@@ -1,25 +1,25 @@
 { pkgs, ... }:
 
 {
-  # Enable Hyprland
-  programs.hyprland = {
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
     enable = true;
-    withUWSM = true;
-  };
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    wireplumber.enable = true;
+    # If you want to use JACK applications, uncomment this
+    # jack.enable = true;
 
-  programs.hyprlock.enable = true;
-  services.hypridle.enable = true;
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    # media-session.enable = true;
+  };
 
   environment.systemPackages = with pkgs; [
-    pyprland
-    hyprpicker
-    hyprcursor
-    hyprlock
-    hypridle
-    hyprpaper
-    hyprsunset
-    hyprpolkitagent
+    pamixer
+    pavucontrol
   ];
 }
