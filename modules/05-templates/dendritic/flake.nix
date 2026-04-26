e21ba82ba033ef592022021176f62ea0ade647d9
Fynn Jansen <fynn@jansen-home.de>
2026-04-26 14:22:04 +0200

    fix syntax

--- modules/05-templates/dendritic/flake.nix
+++ modules/05-templates/dendritic/flake.nix
@@ -2,37 +2,37 @@
 
 in {
   flake.templates.dendritic = {
-    path = self.templateFlake.dendritic
+    path = self.templateFlake.dendritic;
     description = "Dendritic pattern template using flake-parts";
   };
 
   flake.templateFlake.dendritic = {
     description = "NixOS configuration";
-  
+
     inputs = {
       # Nixpkgs-unstable
       nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-      
+
       # Disko
       disko.url = "github:nix-community/disko";
       disko.inputs.nixpkgs.follows = "nixpkgs";
-  
+
       # Flake parts
       flake-parts.url = "github:hercules-ci/flake-parts";
       flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
-      
+
       # Home Manager
       home-manager.url = "github:nix-community/home-manager";
       home-manager.inputs.nixpkgs.follows = "nixpkgs";
-      
+
       # Import Tree
       import-tree.url = "github:vic/import-tree";
-  
+
       # Flake Schemas (currently unused<q)
       flake-schemas.url = "github:DeterminateSystems/flake-schemas";
-  
+
     };
-  
+
     outputs = { flake-parts, flake-schemas, ... } @ inputs: flake-parts.lib.mkFlake { inherit inputs; } {
       # flake.schemas = flake-schemas.schemas;
       imports = [
