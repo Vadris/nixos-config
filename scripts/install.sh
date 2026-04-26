01bcbcb4a4d75a5cf5734694d6952ce6fb61e3fd
Fynn Jansen <fynn@jansen-home.de>
2026-04-26 23:36:56 +0200

    pipefail

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #/bin/env bash
+set -o pipefail
+
 # TODO: querry input from user
-hostname="-"
-disk="-"
 sudo nix run 'github:nix-community/disko/latest#disko-install' -- --flake github:vadris/nixos-config#$hostname --disk nixos $disk
 
 git clone https://github.com/vadris/nixos-config /etc/nixos/
--- scripts/system-update.sh
+++ scripts/system-update.sh
@@ -1,1 +1,2 @@
 #/bin/env bash
+set -o pipefail
