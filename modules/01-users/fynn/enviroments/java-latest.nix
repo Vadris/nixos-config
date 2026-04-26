e66935d7de8e84fe53358dd727b87e313fead912
Fynn Jansen <fynn@jansen-home.de>
2026-04-26 15:44:43 +0200

    Shell testing

--- modules/01-users/fynn/enviroments/java-latest.nix
+++ modules/01-users/fynn/enviroments/java-latest.nix
@@ -1,3 +1,19 @@
 { self, inputs, ... }: {
-  
+  perSystem = {pkgs, config, ...}: {
+    devShells.java-latest = pkgs.mkShell {
+      packages = with pkgs; [
+        jdk25        # The latest General Availability release
+        gradle       # Or gradle, ant, etc.
+        jdt-language-server
+      ];
+      env = {
+        JAVA_HOME = pkgs.jdk26.home;
+      };
+      # 3. Automation on entry
+      shellHook = ''
+        echo "☕ Java Development Environment"
+        java -version
+      '';
+    };
+  };
 }
