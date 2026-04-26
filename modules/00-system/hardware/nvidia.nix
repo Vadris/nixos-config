856bd0df847f833542f539ac50a2d29f60b18842
Fynn Jansen <fynn@jansen-home.de>
2026-04-26 02:48:08 +0200

    more nvidia

---
+++ modules/00-system/hardware/nvidia.nix
@@ -0,0 +1,8 @@
+{ self, inputs, ... }: {
+  flake.nixosModules.nvidia =  { pkgs, lib, config ...}: {
+    hardware.graphics.enable = true;
+    services.xserver.videoDrivers = [ "nvidia" ];
+    hardware.nvidia.open = true;  # see the note above
+    hardware.opengl.driSupport32Bit = true;
+  };
+}
