817e85ecbc3055a2d61d7a15f3fee874d2d9a678
Fynn Jansen <fynn@jansen-home.de>
2026-04-26 23:40:02 +0200

    Fix more syntax errors

--- modules/00-system/kernels/[NM, F] cachyos-latest.nix
+++ modules/00-system/kernels/[NM, F] cachyos-latest.nix
@@ -1,5 +1,5 @@
 { self, inputs, ... }: {
-  flake.nixosModules.kernels.cachyos-latest =  {
+  flake.nixosModules.kernels.cachyos-latest = {pkgs, ...}: {
     boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
   };
 }
