104460b544b47345a73dcde177f863a480bb04db
Fynn Jansen <fynn@jansen-home.de>
2026-04-28 03:39:35 +0200

    Install script needs to be done correctly

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,9 @@
 set -o pipefail
 
 # TODO: querry input from user
-sudo nix run 'github:nix-community/disko/latest#disko-install' -- --flake github:vadris/nixos-config#$hostname --disk nixos $disk
+# sudo nix run 'github:nix-community/disko/latest#disko-install' -- --flake github:vadris/nixos-config#$hostname --disk nixos $disk
 
-git clone https://github.com/vadris/nixos-config /etc/nixos/
+# nixos-generate-config --no-flakes --show-hardware-config > hardware-configuration.nix
+
+# git clone https://github.com/vadris/nixos-config /etc/nixos/
 
