73c12ab0cc8d7c3bd223e1ced9e96ebae43f332d
Fynn Jansen <fynn@jansen-home.de>
2026-04-29 20:13:29 +0200

    Fixed snytax error

---
+++ modules/01-users/fynn/programs [F]/graphical/[NM, HM, F] thunderbird.nix
@@ -0,0 +1,12 @@
+{ self, inputs, ... }: {
+  flake.nixosModules.thunderbird = { pkgs, ...}: {
+    enviroment.systemPackages = [
+      pkgs.thunderbird
+    ];
+  };
+
+  flake.homeModules.fynn-thunderbird = {
+    programs.thunderbird = {
+      enable = true;
+    };
+}
