89322e999d45fbc7dec1cfe506618aaa9fb165c0
Fynn Jansen <fynn@jansen-home.de>
2026-05-01 13:29:57 +0200

    hyprpaper (HM, F)

--- modules/01-users/fynn/programs [F]/graphical/[NM, HM, F] hyprpaper.nix
+++ modules/01-users/fynn/programs [F]/graphical/[NM, HM, F] hyprpaper.nix
@@ -8,4 +8,21 @@ in {
       pkgs.hyprpaper
     ];
   };
+
+  flake.homeModules.fynn-hyprpaper = { pkgs, lib, config, ... }: {
+    imports = [
+      self.homeModules.theme
+    ];
+
+    services.hyprpaper = {
+      enable = true;
+      settings = {
+        splash = false;
+        # TODO: Map wallpapers from theme config
+        wallpapers = [
+
+        ];
+      };
+    }
+  }
 }
